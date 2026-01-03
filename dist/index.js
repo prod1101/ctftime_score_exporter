c44f1b3f7e15fa518e298c44bcd95cdc9bd0080c
prod1101 <git@prod1101.com>
2026-01-03 01:49:53 +0100

    ..

--- dist/index.js
+++ dist/index.js
@@ -26870,11 +26870,12 @@ async function run() {
         const end_year = core.getInput('end_year') === 'present'
             ? new Date().getFullYear() + 1
             : new Date(parseInt(core.getInput('end_year'), 10), 1, 1).getFullYear();
-        const interesting_years = (0, utils_1.range)(start_year, end_year);
-        // .filter(
-        //   year => {team_data.rating[year.toString()].rating_points !== undefined}
-        // )
-        // .reverse()
+        const interesting_years = (0, utils_1.range)(start_year, end_year)
+            .filter(year => {
+            const r = team_data.rating?.[String(year)];
+            return r?.rating_points !== undefined;
+        })
+            .reverse();
         core.info('interesting_years:' + JSON.stringify(interesting_years));
         const percentile_colors = core.getInput('percentile_colors').toLowerCase() === 'true';
         let comp_data = '';
