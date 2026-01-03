82d2407fdfb4d8993e4d691cd5c8209fb85493d9
prod1101 <git@prod1101.com>
2026-01-03 01:44:02 +0100

    ..

--- src/main.ts
+++ src/main.ts
@@ -34,11 +34,10 @@ export async function run(): Promise<void> {
         ? new Date().getFullYear() + 1
         : new Date(parseInt(core.getInput('end_year'), 10), 1, 1).getFullYear()
     const interesting_years = range(start_year, end_year)
-      .filter(year => {
-        core.info('year:' + year.toString())
-        team_data.rating[year.toString()].rating_points !== undefined
-      })
-      .reverse()
+    // .filter(
+    //   year => {team_data.rating[year.toString()].rating_points !== undefined}
+    // )
+    // .reverse()
     core.info('interesting_years:' + JSON.stringify(interesting_years))
     const percentile_colors =
       core.getInput('percentile_colors').toLowerCase() === 'true'
